90e98f7e9414a351ef3fd7a4090bfb3fd1638f34
rima9s <33207744+rima9s@users.noreply.github.com>
2018-11-18 13:39:38 -0800

    updated spacing

--- bot_toast.py
+++ bot_toast.py
@@ -9,6 +9,7 @@ print(TOKEN)
 
 client = discord.Client()
 
+
 @client.event
 async def on_message(message):
     # prevent bot replying to itself
@@ -18,6 +19,7 @@ async def on_message(message):
     if message.content.startswith('!hello'):
         msg = 'hello {0.author.mention}'.format(message)
         await client.send_message(message.channel, msg)
+
 
 @client.event
 async def on_ready():
