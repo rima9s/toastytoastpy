2e25170b61acae050963044178f17c4def15341e
rima9s <33207744+rima9s@users.noreply.github.com>
2018-11-20 23:31:02 -0800

    8-ball test added

--- bot_toast.py
+++ bot_toast.py
@@ -1,14 +1,17 @@
-#!python36
+#python3.6 required
+import random
+import asyncio
+import aiohttp
+import json
 
 import discord
-
 from bot_token import imtoken
 
 TOKEN = imtoken
 
-print(TOKEN)
+#client = discord.Client()
 
-client = discord.Client()
+BOT_PREFIX = ("?", "!")
 
 
 @client.event
@@ -21,6 +24,20 @@ async def on_message(message):
         msg = 'hello {0.author.mention}'.format(message)
         await client.send_message(message.channel, msg)
 
+@client.command(name='8ball',
+                description="Answers a yes/no question.",
+                brief="Answers from the beyond.",
+                aliases=['eight_ball', 'eightball', '8-ball'],
+                pass_context=True)
+async def eight_ball(context):
+    possible_responses = [
+        'That is a resounding no',
+        'Mmm. Really not looking likely mate',
+        'Wee too hard to say',
+        'It is a possibility',
+        'Most certainly so',
+    ]
+    await client.say(random.choice(possible_responses) + ", " context.message.author.mention)
 
 @client.event
 async def on_ready():
